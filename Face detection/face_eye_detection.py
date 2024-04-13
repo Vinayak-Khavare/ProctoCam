27fc18a55c4b1baf3de3f2fa4e341b32099d01eb
Vinayak khavare <vinayakkhavare62@gmail.com>
2024-04-13 18:28:37 +0530

    implemented eye gaze tells if user is not looking

--- Face detection/face_eye_detection.py
+++ Face detection/face_eye_detection.py
@@ -1,6 +1,6 @@
 import cv2
 
-# For the face and eye detected load the Haar cascades
+# For the face and eye detection load the Haar cascades
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
@@ -17,10 +17,23 @@ def detect_eyes_and_face(gray_frame, colored_frame):
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
             center = (x+ex+ew // 2, y+ey+eh // 2)
             cv2.circle(colored_frame, center, 2, (0, 0, 255), -1)
+        
+        if eye_gaze(colored_frame, x, y, w, h):
+            # Notify the user.
+            print("User is looking away from the camera.")
     
     return colored_frame
 
-    
+def eye_gaze(frame, x, y, w, h):
+    eye_region = frame[y:y+h, x:x+w]
+    eye_region_gray = cv2.cvtColor(eye_region, cv2.COLOR_BGR2GRAY)
+    eyes = eye_cascade.detectMultiScale(eye_region_gray, scaleFactor=1.1, minNeighbors=5)
+    # If no eyes are detected, then the user is looking away from the camera.
+    if len(eyes) == 0:
+        return True
+    # Otherwise, the user is looking at the camera.
+    return False
+
 # open webcam
 webcam_capture = cv2.VideoCapture(0)
 print("""Press "q" to Exit.""")
@@ -37,4 +50,3 @@ while(True):
 
 webcam_capture.release()
 cv2.destroyAllWindows()
-    
